9e50db98dfefe14f066dec65ccd1837028c899a7
wagner <wagnerricardonet@gmail.com>
2020-04-28 22:41:25 -0300

    create service eureka-service

--- loja/src/main/java/br/com/wagnerww/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/wagnerww/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.wagnerww.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,11 @@ import br.com.wagnerww.microservice.loja.controller.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+
 	public void realizaCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
 				"http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
