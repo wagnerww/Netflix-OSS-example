a8992843d8713c5847612d97f2d244fb1fa5dc08
wagner <wagnerricardonet@gmail.com>
2020-05-01 09:40:28 -0300

    new config of fornecedor

--- loja/src/main/java/br/com/wagnerww/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/wagnerww/microservice/loja/service/CompraService.java
@@ -1,11 +1,9 @@
 package br.com.wagnerww.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.wagnerww.microservice.loja.client.FornecedorClient;
 import br.com.wagnerww.microservice.loja.controller.dto.CompraDTO;
 import br.com.wagnerww.microservice.loja.controller.dto.InfoFornecedorDTO;
 
@@ -13,15 +11,13 @@ import br.com.wagnerww.microservice.loja.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO compra) {
 
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
-				InfoFornecedorDTO.class);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
-		System.out.println(exchange.getBody().getEndereco());
+		System.out.println(info.getEndereco());
 
 	}
 
